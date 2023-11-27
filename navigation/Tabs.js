0c283528960690355f35943bc29841b7e350b8f1
cold_hailstone <cksn1993@gmail.com>
2023-11-27 15:44:52 +0900

    2.6 Configuring the Navigation

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { Text, View } from 'react-native';
 import Movies from '../screens/Movies';
 import Search from '../screens/Search';
 import Tv from '../screens/Tv';
@@ -7,7 +8,21 @@ import Tv from '../screens/Tv';
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-    <Tab.Navigator>
+    <Tab.Navigator
+        screenOptions={{
+            tabBarStyle: {
+                backgroundColor: 'tomato',
+            },
+            tabBarActiveTintColor: 'red',
+            tabBarInactiveTintColor: 'purple',
+            headerTitleStyle: { color: 'tomato' },
+            headerRight: () => (
+                <View>
+                    <Text>Hello</Text>
+                </View>
+            ),
+        }}
+    >
         <Tab.Screen name='Movies' component={Movies} />
         <Tab.Screen name='TV' component={Tv} />
         <Tab.Screen name='Search' component={Search} />
