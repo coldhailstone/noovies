888676f3d1e76c280f858c7a5ba9c284856b1f13
cold_hailstone <cksn1993@gmail.com>
2023-11-27 16:07:16 +0900

    2.7 Dark Mode

---
+++ colors.js
@@ -0,0 +1,4 @@
+export const YELLOW_COLOR = '#ffa801';
+export const BLACK_COLOR = '#1e272e';
+export const GRAY_100_COLOR = '#d2dae2';
+export const GRAY_200_COLOR = '#808e9b';
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,32 +1,36 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { useColorScheme } from 'react-native';
+import { BLACK_COLOR, GRAY_100_COLOR, GRAY_200_COLOR, YELLOW_COLOR } from '../colors';
 import Movies from '../screens/Movies';
 import Search from '../screens/Search';
 import Tv from '../screens/Tv';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-    <Tab.Navigator
-        screenOptions={{
-            tabBarStyle: {
-                backgroundColor: 'tomato',
-            },
-            tabBarActiveTintColor: 'red',
-            tabBarInactiveTintColor: 'purple',
-            headerTitleStyle: { color: 'tomato' },
-            headerRight: () => (
-                <View>
-                    <Text>Hello</Text>
-                </View>
-            ),
-        }}
-    >
-        <Tab.Screen name='Movies' component={Movies} />
-        <Tab.Screen name='TV' component={Tv} />
-        <Tab.Screen name='Search' component={Search} />
-    </Tab.Navigator>
-);
+const Tabs = () => {
+    const isDark = useColorScheme() === 'dark';
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: isDark ? BLACK_COLOR : 'white',
+                },
+                tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+                tabBarInactiveTintColor: isDark ? GRAY_100_COLOR : GRAY_200_COLOR,
+                headerStyle: {
+                    backgroundColor: isDark ? BLACK_COLOR : 'white',
+                },
+                headerTitleStyle: {
+                    color: isDark ? 'white' : BLACK_COLOR,
+                },
+            }}
+        >
+            <Tab.Screen name='Movies' component={Movies} />
+            <Tab.Screen name='TV' component={Tv} />
+            <Tab.Screen name='Search' component={Search} />
+        </Tab.Navigator>
+    );
+};
 
 export default Tabs;
