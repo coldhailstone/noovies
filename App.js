01bb0d6897279d15c8287002ea349d7d59dd8091
cold_hailstone <cksn1993@gmail.com>
2023-11-27 21:57:04 +0900

    2.15 Themes

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { useAssets } from 'expo-asset';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
+import { useColorScheme } from 'react-native';
 import Root from './navigation/Root';
+import { ThemeProvider } from 'styled-components/native';
+import { darkTheme, lightTheme } from './styled';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -18,13 +21,16 @@ export default function App() {
         }
     }, [assets, fonts]);
 
+    const isDark = useColorScheme() === 'dark';
     if (!assets || !fonts) {
         return null;
     }
 
     return (
-        <NavigationContainer onReady={onLayoutRootView}>
-            <Root />
-        </NavigationContainer>
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <NavigationContainer onReady={onLayoutRootView}>
+                <Root />
+            </NavigationContainer>
+        </ThemeProvider>
     );
 }
