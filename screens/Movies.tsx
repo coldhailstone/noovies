bb4a9621c175a69371d15616e169e7c84e7620db
cold_hailstone <cksn1993@gmail.com>
2023-11-30 22:09:29 +0900

    3.2 Movies Screen part One

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,22 +1,45 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import styled from 'styled-components/native';
+import Swiper from 'react-native-web-swiper';
+import { Dimensions } from 'react-native';
 
-const Btn = styled.TouchableOpacity`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
+const Container = styled.ScrollView`
     background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-    color: ${(props) => props.theme.textColor};
+const View = styled.View`
+    flex: 1;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: { navigate } }) => (
-    <Btn onPress={() => navigate('Stack', { screen: 'Three' })}>
-        <Title>Movies</Title>
-    </Btn>
-);
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: { navigate } }) => {
+    const getNowPlaying = () => {
+        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer f8d2e8ac76a2e902ea6930dadf78bfd8',
+            },
+        });
+    };
+
+    return (
+        <Container>
+            <Swiper
+                loop
+                timeout={3.5}
+                controlsEnabled={false}
+                containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}
+            >
+                <View style={{ backgroundColor: 'red' }}></View>
+                <View style={{ backgroundColor: 'blue' }}></View>
+                <View style={{ backgroundColor: 'red' }}></View>
+                <View style={{ backgroundColor: 'blue' }}></View>
+            </Swiper>
+        </Container>
+    );
+};
 
 export default Movies;
