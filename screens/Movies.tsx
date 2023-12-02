f978b780a448120d4366fb9c1088f83726961644
cold_hailstone <cksn1993@gmail.com>
2023-12-02 16:51:41 +0900

    3.4 Movies Screen part Three

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,8 +1,8 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
-import Swiper from 'react-native-web-swiper';
-import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+import { ActivityIndicator, Dimensions, StyleSheet, useColorScheme } from 'react-native';
 import { makeImgPath } from '../utils';
 import { BlurView } from 'expo-blur';
 
@@ -20,11 +20,40 @@ const Loader = styled.View`
 
 const BgImg = styled.Image``;
 
-const Title = styled.Text``;
+const Poster = styled.Image`
+    width: 100px;
+    height: 160px;
+    border-radius: 5px;
+`;
+
+const Title = styled.Text`
+    font-size: 16px;
+    font-weight: 600;
+    color: white;
+`;
+const Wrapper = styled.View`
+    flex-direction: row;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+const Column = styled.View`
+    width: 40%;
+    margin-left: 15px;
+`;
+const OverView = styled.Text`
+    margin-top: 10px;
+    color: rgba(255, 255, 255, 0.8);
+`;
+const Votes = styled(OverView)`
+    margin-top: 5px;
+    font-size: 12px;
+`;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: { navigate } }) => {
+    const isDark = useColorScheme() === 'dark';
     const [loading, setLoading] = useState(true);
     const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
     const getNowPlaying = async () => {
@@ -47,9 +76,12 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: {
     ) : (
         <Container>
             <Swiper
+                horizontal
                 loop
-                timeout={3.5}
-                controlsEnabled={false}
+                autoplay
+                autoplayTimeout={3.5}
+                showsButtons={false}
+                showsPagination={false}
                 containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}
             >
                 {nowPlayingMovies.map((movie) => (
@@ -58,8 +90,21 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: {
                             style={StyleSheet.absoluteFill}
                             source={{ uri: makeImgPath(movie.backdrop_path) }}
                         />
-                        <BlurView intensity={30} style={StyleSheet.absoluteFill}>
-                            <Title>{movie.original_title}</Title>
+                        <BlurView
+                            tint={isDark ? 'dark' : 'light'}
+                            intensity={30}
+                            style={StyleSheet.absoluteFill}
+                        >
+                            <Wrapper>
+                                <Poster source={{ uri: makeImgPath(movie.poster_path) }}></Poster>
+                                <Column>
+                                    <Title>{movie.original_title}</Title>
+                                    {movie.vote_average > 0 ? (
+                                        <Votes>{movie.vote_average.toFixed(1)}/10</Votes>
+                                    ) : null}
+                                    <OverView>{movie.overview.slice(0, 90)}...</OverView>
+                                </Column>
+                            </Wrapper>
                         </BlurView>
                     </View>
                 ))}
