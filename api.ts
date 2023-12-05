1a25039600fab0d52b71e4c63cfffcfdaca82500
cold_hailstone <cksn1993@gmail.com>
2023-12-05 12:40:16 +0900

    3.16 TV Screen part One

--- api.ts
+++ api.ts
@@ -28,19 +28,32 @@ export interface MovieResponse extends BaseResponse {
     results: Moive[];
 }
 
-const trending = () =>
-    fetch(`${BASE_URL}/trending/movie/week?language=ko-KR&api_key=${API_KEY}`).then((res) =>
-        res.json()
-    );
+export const moviesApi = {
+    trending: () =>
+        fetch(`${BASE_URL}/trending/movie/week?language=ko-KR&api_key=${API_KEY}`).then((res) =>
+            res.json()
+        ),
+    upcoming: () =>
+        fetch(`${BASE_URL}/movie/upcoming?language=ko-KR&page=1&api_key=${API_KEY}`).then((res) =>
+            res.json()
+        ),
+    nowPlaying: () =>
+        fetch(`${BASE_URL}/movie/now_playing?language=ko-KR&page=1&api_key=${API_KEY}`).then(
+            (res) => res.json()
+        ),
+};
 
-const upcoming = () =>
-    fetch(`${BASE_URL}/movie/upcoming?language=ko-KR&page=1&api_key=${API_KEY}`).then((res) =>
-        res.json()
-    );
-
-const nowPlaying = () =>
-    fetch(`${BASE_URL}/movie/now_playing?language=ko-KR&page=1&api_key=${API_KEY}`).then((res) =>
-        res.json()
-    );
-
-export const moviesApi = { trending, upcoming, nowPlaying };
+export const tvApi = {
+    trending: () =>
+        fetch(`${BASE_URL}/trending/tv/week?language=ko-KR&api_key=${API_KEY}`).then((res) =>
+            res.json()
+        ),
+    airingToday: () =>
+        fetch(`${BASE_URL}/tv/airing_today?language=ko-KR&api_key=${API_KEY}`).then((res) =>
+            res.json()
+        ),
+    topRated: () =>
+        fetch(`${BASE_URL}/tv/top_rated?language=ko-KR&api_key=${API_KEY}`).then((res) =>
+            res.json()
+        ),
+};
--- screens/Tv.tsx
+++ screens/Tv.tsx
@@ -1,10 +1,64 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { FlatList, ScrollView, Text, View } from 'react-native';
+import { useQuery } from 'react-query';
+import { tvApi } from '../api';
+import Loader from '../components/Loader';
+import VMedia from '../components/VMedia';
 
-const Tv = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Tv</Text>
-    </View>
-);
+const Tv = () => {
+    const { isLoading: todayLoading, data: todayData } = useQuery(
+        ['tv', 'today'],
+        tvApi.airingToday
+    );
+    const { isLoading: topLoading, data: topData } = useQuery(['tv', 'top'], tvApi.topRated);
+    const { isLoading: trendingLoading, data: trendingData } = useQuery(
+        ['tv', 'trending'],
+        tvApi.trending
+    );
+    const loading = todayLoading || topLoading || trendingLoading;
+
+    return loading ? (
+        <Loader />
+    ) : (
+        <ScrollView>
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={trendingData.results}
+                renderItem={({ item }) => (
+                    <VMedia
+                        posterPath={item.poster_path}
+                        originalTitle={item.original_name}
+                        voteAverage={item.vote_average}
+                    />
+                )}
+            />
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={todayData.results}
+                renderItem={({ item }) => (
+                    <VMedia
+                        posterPath={item.poster_path}
+                        originalTitle={item.original_name}
+                        voteAverage={item.vote_average}
+                    />
+                )}
+            />
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={topData.results}
+                renderItem={({ item }) => (
+                    <VMedia
+                        posterPath={item.poster_path}
+                        originalTitle={item.original_name}
+                        voteAverage={item.vote_average}
+                    />
+                )}
+            />
+        </ScrollView>
+    );
+};
 
 export default Tv;
